cf42e3222178bc66b25a69e59771ff7929d91cd3
ashishsahoo18 <sahooashish673@gmail.com>
2026-06-26 16:00:55 -0500

    Update views.py

--- Block-Rock-Ecommerce/blockrock/accounts/views.py
+++ Block-Rock-Ecommerce/blockrock/accounts/views.py
@@ -1,3 +1,76 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from django.contrib.auth.models import User
+
+from django.contrib.auth import authenticate, login, logout
+
+
+def signup(request):
+
+    if request.method == "POST":
+
+        username = request.POST['username']
+
+        email = request.POST['email']
+
+        password = request.POST['password']
+
+
+        user = User.objects.create_user(
+
+            username=username,
+
+            email=email,
+
+            password=password
+
+        )
+
+
+        user.save()
+
+
+        return redirect('login')
+
+
+    return render(request,'signup.html')
+
+
+
+
+def login_user(request):
+
+    if request.method == "POST":
+
+
+        username = request.POST['username']
+
+        password = request.POST['password']
+
+
+        user = authenticate(
+
+            username=username,
+
+            password=password
+
+        )
+
+
+        if user:
+
+            login(request,user)
+
+            return redirect('/')
+
+
+    return render(request,'login.html')
+
+
+
+
+def logout_user(request):
+
+    logout(request)
+
+    return redirect('login')
