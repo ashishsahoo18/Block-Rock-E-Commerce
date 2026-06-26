11d8bcbe985d54d7bca4da7d305c7247f3ca584d
ashishsahoo18 <sahooashish673@gmail.com>
2026-06-26 16:15:06 -0500

    replace code

--- Block-Rock-Ecommerce/blockrock/products/models.py
+++ Block-Rock-Ecommerce/blockrock/products/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+
+    name = models.CharField(max_length=200)
+
+    description = models.TextField()
+
+    price = models.IntegerField()
+
+    discount = models.IntegerField(default=0)
+
+    category = models.CharField(max_length=100)
+
+    brand = models.CharField(max_length=100)
+
+    stock = models.IntegerField(default=0)
+
+    rating = models.FloatField(default=0)
+
+    image = models.ImageField(upload_to='products/')
+
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+
+        return self.name
--- Block-Rock-Ecommerce/blockrock/products/views.py
+++ Block-Rock-Ecommerce/blockrock/products/views.py
@@ -1,3 +1,66 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Product
+
+
+
+def products(request):
+
+
+    query = request.GET.get('search')
+
+
+    if query:
+
+
+        products = Product.objects.filter(
+
+            name__icontains=query
+
+        )
+
+
+    else:
+
+
+        products = Product.objects.all()
+
+
+
+    return render(
+
+        request,
+
+        'products.html',
+
+        {
+
+            'products':products
+
+        }
+
+    )
+
+
+
+
+
+def product_detail(request,id):
+
+
+    product = Product.objects.get(id=id)
+
+
+    return render(
+
+        request,
+
+        'product_detail.html',
+
+        {
+
+            'product':product
+
+        }
+
+    )
