286b505d3a5564c713b744565a51277934e3f605
ashishsahoo18 <sahooashish673@gmail.com>
2026-08-20 20:33:26 -0500

    Update settings.py

--- Block-Rock-Ecommerce/blockrock/blockrock/settings.py
+++ Block-Rock-Ecommerce/blockrock/blockrock/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'blockrock.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,7 +122,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static"
