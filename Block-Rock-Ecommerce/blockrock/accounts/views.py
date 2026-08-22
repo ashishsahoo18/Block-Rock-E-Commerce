dd57477826bee02280c90ea63bfe717cf5bcc32a
ashishsahoo18 <sahooashish673@gmail.com>
2026-08-21 21:18:47 -0500

    Update views.py

--- Block-Rock-Ecommerce/blockrock/accounts/views.py
+++ Block-Rock-Ecommerce/blockrock/accounts/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 
 from django.contrib.auth import authenticate, login, logout
+from django.utils.http import url_has_allowed_host_and_scheme
 
 
 def signup(request):
@@ -61,10 +62,13 @@ def login_user(request):
 
             login(request,user)
 
+            next_url = request.POST.get('next') or request.GET.get('next')
+            if next_url and url_has_allowed_host_and_scheme(next_url, {request.get_host()}):
+                return redirect(next_url)
             return redirect('/')
 
 
-    return render(request,'login.html')
+    return render(request, 'login.html', {'next': request.GET.get('next', '')})
 
 
 
