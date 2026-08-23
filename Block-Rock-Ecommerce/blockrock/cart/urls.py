4f68465b7b5dbf40f12379fe1c7023bcec5ea273
ashishsahoo18 <sahooashish673@gmail.com>
2026-08-22 19:16:23 -0500

    Update urls.py

--- Block-Rock-Ecommerce/blockrock/cart/urls.py
+++ Block-Rock-Ecommerce/blockrock/cart/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+
 from . import views
 
-
 urlpatterns = [
-
+    path('', views.cart_detail, name='cart_detail'),
+    path('add/<slug:slug>/', views.add_to_cart, name='add_to_cart'),
+    path('update/<int:item_id>/', views.update_cart_item, name='update_cart_item'),
+    path('remove/<int:item_id>/', views.remove_from_cart, name='remove_from_cart'),
 ]
