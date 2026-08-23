6a43596f72d201a172993d24b6a1c6326623def3
ashishsahoo18 <sahooashish673@gmail.com>
2026-08-22 19:15:44 -0500

    Update admin.py

--- Block-Rock-Ecommerce/blockrock/cart/admin.py
+++ Block-Rock-Ecommerce/blockrock/cart/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Cart, CartItem, Wishlist, WishlistItem
+
+
+class CartItemInline(admin.TabularInline):
+    model = CartItem
+    extra = 0
+    readonly_fields = ('created_at', 'updated_at')
+
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('user', 'item_count', 'updated_at')
+    search_fields = ('user__username', 'user__email')
+    inlines = (CartItemInline,)
+
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ('product', 'cart', 'quantity', 'updated_at')
+    search_fields = ('product__name', 'cart__user__username')
+
+
+@admin.register(Wishlist)
+class WishlistAdmin(admin.ModelAdmin):
+    list_display = ('user', 'updated_at')
+    search_fields = ('user__username', 'user__email')
+
+
+@admin.register(WishlistItem)
+class WishlistItemAdmin(admin.ModelAdmin):
+    list_display = ('product', 'wishlist', 'created_at')
+    search_fields = ('product__name', 'wishlist__user__username')
