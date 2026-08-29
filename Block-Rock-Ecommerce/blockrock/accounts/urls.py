cdf3494c065a5f5694d02d2bee19589bddfaedf5
ashishsahoo18 <sahooashish673@gmail.com>
2026-08-29 09:57:06 -0500

    Update urls.py

--- Block-Rock-Ecommerce/blockrock/accounts/urls.py
+++ Block-Rock-Ecommerce/blockrock/accounts/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout'),
+    path('newsletter/subscribe/', views.subscribe_newsletter, name='newsletter_subscribe'),
     path('account/', views.account, name='account'),
     path('profile/', views.profile, name='profile'),
     path('profile/edit/', views.profile_edit, name='profile_edit'),
