9bed000c99fc79320e438b96b12df39ba84f7922
ashishsahoo18 <sahooashish673@gmail.com>
2026-08-29 09:56:47 -0500

    Update admin.py

--- Block-Rock-Ecommerce/blockrock/accounts/admin.py
+++ Block-Rock-Ecommerce/blockrock/accounts/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Subscriber
+
+
+@admin.register(Subscriber)
+class SubscriberAdmin(admin.ModelAdmin):
+    list_display = ('email', 'subscribed_at', 'is_active')
+    list_filter = ('is_active',)
+    search_fields = ('email',)
+    ordering = ('-subscribed_at',)
+    actions = ('mark_active', 'mark_inactive')
+
+    @admin.action(description='Reactivate selected subscribers')
+    def mark_active(self, request, queryset):
+        queryset.update(is_active=True)
+
+    @admin.action(description='Deactivate selected subscribers')
+    def mark_inactive(self, request, queryset):
+        queryset.update(is_active=False)
