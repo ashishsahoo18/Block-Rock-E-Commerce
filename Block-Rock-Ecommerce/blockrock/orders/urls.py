8777f87b96802e3accdc39df9394f4879cfc69fc
ashishsahoo18 <sahooashish673@gmail.com>
2026-09-15 10:02:15 -0500

    Update urls.py

--- Block-Rock-Ecommerce/blockrock/orders/urls.py
+++ Block-Rock-Ecommerce/blockrock/orders/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
-
+    path('', views.order_list, name='order_list'),
+    path('<str:order_number>/', views.order_detail, name='order_detail'),
+    path('<str:order_number>/confirmation/', views.order_confirmation, name='order_confirmation'),
 ]
