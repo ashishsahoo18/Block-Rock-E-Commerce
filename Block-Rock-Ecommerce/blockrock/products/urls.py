bc6735928ffb9daed687a9e933e59c6e0903b338
ashishsahoo18 <sahooashish673@gmail.com>
2026-09-26 19:30:52 -0500

    Update urls.py

--- Block-Rock-Ecommerce/blockrock/products/urls.py
+++ Block-Rock-Ecommerce/blockrock/products/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
     path('', views.product_list, name='products'),
+    path('api/search-autocomplete/', views.search_autocomplete, name='search_autocomplete'),
     path('<slug:slug>/', views.product_detail, name='product_detail'),
 ]
